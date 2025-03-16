d32eb967d7a4d7f5e29b0ac52902edb3e4cf2686
Anton Ngan <anton.an.ngan@gmail.com>
2025-03-16 23:36:20 +1100

    Add a slider to change grid size dynamically

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Etch-a-sketch
+
+https://gsytnik.github.io/odin-etch-a-sketch/
+https://issakass.github.io/odin-etch-a-sketch/
+https://danyapac3.github.io/etch-a-sketch/
--- etch.js
+++ etch.js
@@ -3,12 +3,33 @@ let gridHeight = 16;
 const whiteBoard = document.querySelector(".whiteboard");
 const whiteBoardProperties = whiteBoard.getBoundingClientRect();
 
-const squareArea = (whiteBoard.clientHeight ** 2) / (gridHeight ** 2);
-const squareHeight = Math.sqrt(squareArea);
+let slider = document.querySelector(".slider");
+let value = document.querySelector(".slider-value");
 
-for (let i = 0; i < (gridHeight**2); i++) {
-    const newSquare = document.createElement("div");
-    newSquare.classList.add(".square");
-    whiteBoard.appendChild(newSquare);
-    newSquare.setAttribute("style", `width: ${squareHeight}px; height: ${squareHeight}px`);
+value.textContent = `${slider.value} x ${slider.value}`;
+
+slider.addEventListener("input", () =>
+    {
+        value.textContent = `${slider.value} x ${slider.value}`;
+        gridHeight = slider.value;
+        updateGrid();
+    }
+)
+
+function updateGrid () {
+
+    whiteBoard.replaceChildren();
+    const squareArea = (whiteBoard.clientHeight ** 2) / (gridHeight ** 2);
+    const squareHeight = Math.sqrt(squareArea); 
+
+    for (let i = 0; i < (gridHeight**2); i++) {
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("square");
+        whiteBoard.appendChild(newSquare);
+        newSquare.setAttribute("style", `width: ${squareHeight}px; height: ${squareHeight}px`);
+    
+        newSquare.addEventListener("mouseover", (e) => {
+            e.currentTarget.style.backgroundColor = "black";
+        })
+    }
 }
