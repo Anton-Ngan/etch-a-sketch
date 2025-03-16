16b56a42eecfeefe22c3bc341b0e6ec4cb8f4230
Anton Ngan <anton.an.ngan@gmail.com>
2025-03-17 01:02:36 +1100

    Implement the clear and grid button

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Etch-a-sketch
 
+## Credits
+* <a target="_blank" href="https://icons8.com/icon/8112/close">Close</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+* <a target="_blank" href="https://icons8.com/icon/o15Kxsr7cBWT/health-data">Grid</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+* <a target="_blank" href="https://icons8.com/icon/59856/pencil">Draw</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+* <a target="_blank" href="https://icons8.com/icon/cDmb6gHhczNC/rainbow">Rainbow</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+* <a target="_blank" href="https://icons8.com/icon/78791/grayscale">Grayscale</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+* <a target="_blank" href="https://icons8.com/icon/11637/erase">Erase</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+## Inspiration
 https://gsytnik.github.io/odin-etch-a-sketch/
 https://issakass.github.io/odin-etch-a-sketch/
 https://danyapac3.github.io/etch-a-sketch/
--- etch.js
+++ etch.js
@@ -1,22 +1,46 @@
 let gridHeight = 16;
 
 const whiteBoard = document.querySelector(".whiteboard");
-const whiteBoardProperties = whiteBoard.getBoundingClientRect();
+const slider = document.querySelector(".slider");
+const sliderValue = document.querySelector(".slider-value");
+const clearButton = document.querySelector(".clear-icon");
+const gridButton = document.querySelector(".grid-icon");
 
-let slider = document.querySelector(".slider");
-let value = document.querySelector(".slider-value");
+let gridSwitch = false;
 
-value.textContent = `${slider.value} x ${slider.value}`;
+function toggleGrid() {
+    gridButton.addEventListener("click", () => {
+        const squares = document.querySelectorAll(".square");
+        squares.forEach( (square) => square.classList.toggle("active-border"))
+    })
+}
 
-slider.addEventListener("input", () =>
-    {
-        value.textContent = `${slider.value} x ${slider.value}`;
-        gridHeight = slider.value;
-        updateGrid();
-    }
-)
+// border: 1px solid whitesmoke;
+
+function toggleClear() {
+    clearButton.addEventListener("click", () => {
+        updateGrid()
+    })
+}
+
+function updateSliderValue() {
+    sliderValue.textContent = `${slider.value} x ${slider.value}`;
+    slider.addEventListener("input", () =>
+        {
+            sliderValue.textContent = `${slider.value} x ${slider.value}`;
+            gridHeight = slider.value;
+            updateGrid();
+        }
+    )
+}
 
 function updateGrid () {
+
+    let gridStatus = null;
+    const square = document.querySelector(".square");
+    if (square) {
+        gridStatus = square.classList.contains("active-border");
+    }
 
     whiteBoard.replaceChildren();
     const squareArea = (whiteBoard.clientHeight ** 2) / (gridHeight ** 2);
@@ -25,11 +49,23 @@ function updateGrid () {
     for (let i = 0; i < (gridHeight**2); i++) {
         const newSquare = document.createElement("div");
         newSquare.classList.add("square");
+        if (gridStatus) newSquare.classList.add("active-border");
         whiteBoard.appendChild(newSquare);
         newSquare.setAttribute("style", `width: ${squareHeight}px; height: ${squareHeight}px`);
     
         newSquare.addEventListener("mouseover", (e) => {
             e.currentTarget.style.backgroundColor = "black";
+            // e.currentTarget.setAttribute("style", "background-color: black;");
         })
     }
+} 
+
+
+function main() {
+    updateGrid();
+    updateSliderValue();
+    toggleClear();
+    toggleGrid();
 }
+
+main()
