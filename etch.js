0e632840e62d9cf83971723c6af596db22a1a153
Anton Ngan <anton.an.ngan@gmail.com>
2025-03-17 18:13:45 +1100

    Add rainbow mode and eraser functionality

--- etch.js
+++ etch.js
@@ -6,13 +6,51 @@ const slider = document.querySelector(".slider");
 const sliderValue = document.querySelector(".slider-value");
 const clearButton = document.querySelector(".clear-icon");
 const gridButton = document.querySelector(".grid-icon");
+const colorPicker = document.querySelector(".color-wheel");
+const drawButton = document.querySelector(".draw-mode");
+const rainbowButton = document.querySelector(".rainbow-mode");
+const grayscaleButton = document.querySelector(".grayscale-mode");
+const eraseButton = document.querySelector(".erase-icon");
 
 let gridSwitch = false;
 let mouseOn;
 let curColor = DEFAULT_COLOR;
 let gridHeight = DEFAULT_GRID_HEIGHT;
+let activeButton = "draw";
 
+function getRandomColor() {
+    const COLOR_DIGITS = 6;
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < COLOR_DIGITS; i++) {
+      color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+}
 
+function toggleGrayscaleButton() {
+    grayscaleButton.addEventListener("click", (e) => {
+        activeButton = "grayscale";
+    })
+}
+
+function toggleRainbowButton() {
+    rainbowButton.addEventListener("click", (e) => {
+        activeButton = "rainbow";
+    })
+}
+
+function toggleDrawButton() {
+    drawButton.addEventListener("click", (e) => {
+        activeButton = "draw";
+    })
+}
+
+function toggleEraser() {
+    eraseButton.addEventListener("click", (e) => {
+        activeButton = "eraser";
+    })
+}
 
 function toggleGrid() {
     gridButton.addEventListener("click", () => {
@@ -67,11 +105,11 @@ function drawOnGrid () {
         square.addEventListener("mousedown", (e) => {
             mouseOn = true;
             e.preventDefault();
-            e.currentTarget.style.backgroundColor = curColor;
+            e.currentTarget.style.backgroundColor = getColor(square);
         })
 
         square.addEventListener("mouseover", (e) => {
-            if (mouseOn) e.currentTarget.style.backgroundColor = curColor;
+            if (mouseOn) e.currentTarget.style.backgroundColor = getColor(square);
         })
 
         square.addEventListener("mouseup", () => {
@@ -80,12 +118,33 @@ function drawOnGrid () {
     } )
 }
 
+function getColor(square) {
+    switch (activeButton) {
+        case "draw":
+            return colorPicker.value;
+        case "eraser":
+            return "#FFFFFF";
+        case "rainbow":
+            return getRandomColor();
+        case "grayscale":
+            // if (square.style.backgroundColor !== colorPicker.value) {
+            //     square.style.opacity = 0.5;
+            // } else {
+            //     square.style.opacity += 0.1;
+            // }
+            return colorPicker.value;
+    }
+}
 
 function main() {
     updateGrid();
     updateSliderValue();
     toggleClear();
     toggleGrid();
+    toggleEraser();
+    toggleDrawButton();
+    toggleRainbowButton();
+    toggleGrayscaleButton();
 }
 
 main()
